53b6d3c6ac1ab8af43474ad843c779d54d1eb94d
Bhargavvv412 <bhargavvv412@gmail.com>
2025-07-26 11:58:41 +0530

    Ej Upto Ex

---
+++ EJs/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {
+        dayType: "a weekday",
+        advice: "it is time to work hard"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`server is running on ${port}`);
+});
