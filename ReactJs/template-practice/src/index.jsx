f7e4e512c9ad0b62e8d8475e10517f7eddc9396a
Bhargavvv412 <bhargavvv412@gmail.com>
2025-07-31 22:56:12 +0530

    reactjs

---
+++ ReactJs/template-practice/src/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Bhargav";
+const age = 19;
+
+ReactDOM.render(
+  <div>
+    <h1>{name}</h1>
+    <p>{age}</p>
+  </div>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
