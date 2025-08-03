e56cd38ec5631ebc0eb8c43ec18ab9b67b8a8b16
Bhargavvv412 <bhargavvv412@gmail.com>
2025-08-03 23:11:41 +0530

    mapping compelete

--- ReactJs/map-filter-reduce/src/App.jsx
+++ ReactJs/map-filter-reduce/src/App.jsx
@@ -5,6 +5,27 @@ import './App.css'
 
 function App() {
   var numbers = [3, 56, 2, 48, 5];
+  console.log(numbers);
+
+  // maps
+  function double(x) {
+    return x * 2;
+  };
+  const newNum = numbers.map(double);
+  console.log(newNum);
+
+  // filters
+  const filterNum = numbers.filter(function (nums) {
+    return nums > 10;
+  });
+  console.log(filterNum)
+
+  // Reduse
+  const reduceNum = numbers.reduce(function (acc, currNum) {
+    return acc + currNum;
+  })
+
+  console.log(reduceNum);
   return (
     <>
     </>
