777a59c02bc87feb50db468329b8408bb46a5c97
Bhargavvv412 <bhargavvv412@gmail.com>
2025-08-05 21:52:55 +0530

    State impertative delacrative programming

---
+++ ReactJs/State/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  function strike() {
+    document.getElementById("root").style.textDecoration = "line-through";
+  }
+
+  var isDone = true;
+
+  const strikeThrough = { textDecoration: "line-through" };
+
+  return (
+    <>
+      {/* Delacrative programming */}
+      <h1 style={isDone ? strikeThrough : null}>Delacrative Programming</h1>
+      {/* Impertaive programming */}
+      <h1>Impertavie programming</h1>
+      <button onClick={strike}>Change to strike</button>
+
+    </>
+  )
+}
+
+export default App
