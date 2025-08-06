3917f0b95d30675ed3de94b684eb5b0822a06e20
Bhargavvv412 <bhargavvv412@gmail.com>
2025-08-06 22:27:25 +0530

    formr

--- ReactJs/Forms/src/App.jsx
+++ ReactJs/Forms/src/App.jsx
@@ -4,30 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setHeading(name);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello, {headingText}</h1><br />
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder='what is your name'
+        value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
     </>
   )
 }
